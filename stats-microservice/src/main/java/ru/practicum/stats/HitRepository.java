aac5dafb360b68f4351172e4d43810f5db449776
Oleg <ovasilyevfx@mail.ru>
2023-06-15 01:26:15 +0300

    feat:MVP

--- stats-microservice/src/main/java/ru/practicum/stats/HitRepository.java
+++ stats-microservice/src/main/java/ru/practicum/stats/HitRepository.java
@@ -18,13 +18,16 @@ public interface HitRepository extends JpaRepository<Hit, Long> {
 
     List<Hit> findAllByTimestampBefore(LocalDateTime end);
 
+
     @Query(value = "SELECT  DISTINCT on (h.ip) h.id, h.app, h.uri,  h.ip, h.timestamp FROM hits AS h WHERE h.timestamp > :startDate ORDER  BY  h.ip ", nativeQuery = true)
     List<Hit> getHitDistinctAfter(@Param("startDate") LocalDateTime startDate);
+
 
     @Query(value = "SELECT  DISTINCT on (h.ip) h.id, h.app, h.uri,  h.ip, h.timestamp FROM hits AS h WHERE h.timestamp BETWEEN :startDate AND  :endDate" +
             " ORDER  BY  h.ip ", nativeQuery = true)
     List<Hit> getHitDistinctBetween(@Param("startDate") LocalDateTime startDate,
                                     @Param("endDate") LocalDateTime endDate);
+
 
     @Query(value = "SELECT  DISTINCT on (h.ip) h.id, h.app, h.uri,  h.ip, h.timestamp FROM hits AS h WHERE h.timestamp < :endDate ORDER  BY  h.ip ", nativeQuery = true)
     List<Hit> getHitDistinctBefore(@Param("endDate") LocalDateTime endDate);

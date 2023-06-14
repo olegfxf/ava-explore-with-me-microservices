fb3b0baf1569c16bcc888dede4d11eb1102ce6b1
Oleg <ovasilyevfx@mail.ru>
2023-06-15 01:10:03 +0300

    feat:MVP

--- stats-microservice/src/main/java/ru/practicum/stats/HitRepository.java
+++ stats-microservice/src/main/java/ru/practicum/stats/HitRepository.java
@@ -17,17 +17,16 @@ public interface HitRepository extends JpaRepository<Hit, Long> {
 
     List<Hit> findAllByTimestampBefore(LocalDateTime end);
 
-  //  List<Hit> findDistinctByTimestampAfter(LocalDateTime start);
+    List<Hit> findDistinctByTimestampAfter(LocalDateTime start);
 
-    @Query(value = "SELECT  DISTINCT on (h.ip) h.id, h.app, h.uri,  h.ip, h.timestamp FROM hits AS h" +
-            " WHERE h.timestamp > :startDate" +
-            " ORDER  BY  h.ip ", nativeQuery = true)
+
+    @Query(value = "SELECT  DISTINCT on (h.ip) h.id, h.app, h.uri,  h.ip, h.timestamp FROM hits AS h WHERE h.timestamp > :startDate ORDER  BY  h.ip ", nativeQuery = true)
     List<Hit> getHitDistinctAfter(@Param("startDate") LocalDateTime startDate);
 
-    //List<Hit> findDistinctByTimestampIsBetween(LocalDateTime start, LocalDateTime end);
+    List<Hit> findDistinctByTimestampIsBetween(LocalDateTime start, LocalDateTime end);
 
-    @Query(value = "SELECT  DISTINCT on (h.ip) h.id, h.app, h.uri,  h.ip, h.timestamp FROM hits AS h" +
-            " WHERE h.timestamp BETWEEN :startDate AND  :endDate" +
+
+    @Query(value = "SELECT  DISTINCT on (h.ip) h.id, h.app, h.uri,  h.ip, h.timestamp FROM hits AS h WHERE h.timestamp BETWEEN :startDate AND  :endDate" +
             " ORDER  BY  h.ip ", nativeQuery = true)
     List<Hit> getHitDistinctBetween(@Param("startDate") LocalDateTime startDate,
                                     @Param("endDate") LocalDateTime endDate);

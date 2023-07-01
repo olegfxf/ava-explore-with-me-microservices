5ba5a26146f7c4ffab95c0cf72492bec14f99098
Oleg <ovasilyevfx@mail.ru>
2023-07-01 09:39:49 +0300

    feat:Реализовано функционал по ТЗ

--- ewm-service/src/main/java/ru/practicum/mainmicroservice/MainMicroserviseApplication.java
+++ ewm-service/src/main/java/ru/practicum/mainmicroservice/MainMicroserviseApplication.java
@@ -6,8 +6,14 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class MainMicroserviseApplication {
 
+	//public static void main(String[] args) {
+	//	SpringApplication.run(MainPublicApplication1.class, args);
+//	}
 	public static void main(String[] args) {
+
 		SpringApplication.run(MainMicroserviseApplication.class, args);
+
 	}
+
 
 }

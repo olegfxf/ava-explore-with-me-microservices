95ec837e95ca3c4741038bbefd04ee2073d24a09
olegfxf <102871596+olegfxf@users.noreply.github.com>
2023-07-02 21:13:42 +0300

    Update wait-for-it.sh

--- .github/workflows/wait-for-it.sh
+++ .github/workflows/wait-for-it.sh
@@ -61,7 +61,6 @@ wait_for_wrapper()
     WAITFORIT_PID=$!
     trap "kill -INT -$WAITFORIT_PID" INT
     wait $WAITFORIT_PID
-    WAITFORIT_RESULT=$?
     WAITFORIT_RESULT=0
     if [[ $WAITFORIT_RESULT -ne 0 ]]; then
         echoerr "$WAITFORIT_cmdname: timeout occurred after waiting $WAITFORIT_TIMEOUT seconds for $WAITFORIT_HOST:$WAITFORIT_PORT"

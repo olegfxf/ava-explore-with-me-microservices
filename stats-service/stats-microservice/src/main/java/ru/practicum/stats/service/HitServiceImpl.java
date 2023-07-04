b9aabaa4ca8aa85f4ac447341788be35a2c525b6
Oleg <ovasilyevfx@mail.ru>
2023-07-04 07:29:40 +0300

    fix: client

--- stats-service/stats-microservice/src/main/java/ru/practicum/stats/service/HitServiceImpl.java
+++ stats-service/stats-microservice/src/main/java/ru/practicum/stats/service/HitServiceImpl.java
@@ -30,26 +30,11 @@ public class HitServiceImpl implements HitService {
     }
 
     public HitDto save(Stats stats) {
-        List<String> s1 = hitRepository.findAll().stream().map(e->e.getUri()).collect(Collectors.toList());
-        Integer hits0 = s1.stream().filter(e->e.equals(stats.getUri())).collect(Collectors.toList()).size();
-
         HitDto hitDto = HitMapper.toHitDto(hitRepository.save(stats));
 
-        System.out.println("Запись в репо  ////////////");
-        System.out.println("hitDto" + hitDto);
-
-        List<String> s2 = hitRepository.findAll().stream().map(e->e.getUri()).collect(Collectors.toList());
-        Integer hits = s2.stream().filter(e->e.equals(stats.getUri())).collect(Collectors.toList()).size();
+        List<String> uris = hitRepository.findAll().stream().map(e->e.getUri()).collect(Collectors.toList());
+        Integer hits = uris.stream().filter(e->e.equals(stats.getUri())).collect(Collectors.toList()).size();
         hitDto.setHit(hits);
-
-        System.out.println("hits = " + hits0 + "   " + hits);
-        System.out.println("//////////////////////////////");
-        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss");
-        LocalDateTime start = LocalDateTime.parse("2020-05-05 00:00:00", formatter);
-        LocalDateTime end = LocalDateTime.parse("2035-05-05 00:00:00", formatter);
-        List<ViewStats> viewStats1 = hitRepository.getStatsWithoutUri(start, end);
-        viewStats1.stream().forEach(e -> System.out.println("App = " + e.getApp() + "  Url = " + e.getUri() + "  Hits = " + e.getHits()));
-
 
         log.debug("Выполнен запрос на сохранение события");
         return hitDto;
@@ -83,27 +68,9 @@ public class HitServiceImpl implements HitService {
             }
         }
 
-        if (uris.isPresent() && !isUnique && uris.get().get(0).equals("/events")) {
-            System.out.println("Продоставление инф из репо");
-            viewStats.stream().forEach(e -> System.out.println("App = " + e.getApp() + "  Url = " + e.getUri() + "  Hits = " + e.getHits()));
-            System.out.println("//////////////////////////////");
-            List<ViewStats> viewStats1 = hitRepository.getStatsWithoutUri(start, end);
-            viewStats1.stream().forEach(e -> System.out.println("App = " + e.getApp() + "  Url = " + e.getUri() + "  Hits = " + e.getHits()));
-        }
 
- //az = hitRepository.findAll().size();
-//        Stats stats = new Stats();
-//        stats.setIp("127.0.0.0");
-//        stats.setTimestamp(LocalDateTime.now());
-//        stats.setApp("ewm-main-service");
-//        stats.setUri("/events");
-//        System.out.println("az = " + az);
-//        System.out.println(" ZZZ0" + hitRepository.findAll().size());
-        //stats = hitRepository.findBy()
         if (cnt.equals(hitRepository.findAll().size())) {
-            System.out.println(" ZZZ1" + hitRepository.findAll().size());
             hitRepository.save(HitMapper.stats());
-            System.out.println(" ZZZ2" + hitRepository.findAll().size());
         }
         cnt = hitRepository.findAll().size();
 

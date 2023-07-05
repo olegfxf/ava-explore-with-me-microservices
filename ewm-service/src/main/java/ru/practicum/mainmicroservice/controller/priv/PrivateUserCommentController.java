1c8656be31b22594b06549118475d685af623324
Oleg <ovasilyevfx@mail.ru>
2023-07-05 19:48:08 +0300

    feat:comment

--- ewm-service/src/main/java/ru/practicum/mainmicroservice/controller/priv/PrivateUserCommentController.java
+++ ewm-service/src/main/java/ru/practicum/mainmicroservice/controller/priv/PrivateUserCommentController.java
@@ -1,38 +1,43 @@
 package ru.practicum.mainmicroservice.controller.priv;
 
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import ru.practicum.mainmicroservice.dto.CommentDto;
+import ru.practicum.mainmicroservice.service.priv.PrivateUserCommentService;
+
+import javax.validation.Valid;
+import javax.validation.constraints.Positive;
+import java.util.List;
 
 
 @Slf4j
 @RestController
 @RequestMapping("/users/{userId}/comments")
 public class PrivateUserCommentController {
-//
-//    final PrivateUserCommentService privateUserCommentService;
-//
-//    public PrivateUserCommentController(PrivateUserCommentService privateUserCommentService) {
-//        this.privateUserCommentService = privateUserCommentService;
-//    }
-//
-//    @PostMapping("/{eventId}")
-//    public CommentDto saveComment(@Positive @PathVariable Long userId,
-//                                    @Positive @PathVariable Long eventId,
-//                                    @Valid @RequestBody CommentDto commentDto) {
-//        log.info("create comment by user {}", userId);
-//        return privateUserCommentService.saveComment(userId, eventId, commentDto);
-//    }
-//
-//
-//    @GetMapping
-//    public List<CommentDto> getAllCommentsByUser(@Positive @PathVariable Long userId,
-//                                                 @RequestParam(defaultValue = "0") int from,
-//                                                 @RequestParam(defaultValue = "10") int size) {
-//        log.info("get all user {} comments", userId);
-//        return privateUserCommentService.getAllCommentsByUser(userId, from, size);
-//    }
-//
+
+    final PrivateUserCommentService privateUserCommentService;
+
+    public PrivateUserCommentController(PrivateUserCommentService privateUserCommentService) {
+        this.privateUserCommentService = privateUserCommentService;
+    }
+
+    @PostMapping("/{eventId}")
+    public CommentDto saveComment(@Positive @PathVariable Long userId,
+                                  @Positive @PathVariable Long eventId,
+                                  @Valid @RequestBody CommentDto commentDto) {
+        log.info("create comment by user {}", userId);
+        return privateUserCommentService.saveComment(userId, eventId, commentDto);
+    }
+
+
+    @GetMapping
+    public List<CommentDto> getAllCommentsByUser(@Positive @PathVariable Long userId,
+                                                 @RequestParam(defaultValue = "0") int from,
+                                                 @RequestParam(defaultValue = "10") int size) {
+        log.info("get all user {} comments", userId);
+        return privateUserCommentService.getAllCommentsByUser(userId, from, size);
+    }
+
 //
 //    @PatchMapping("/{commentId}")
 //    public CommentDto updateComment(@Positive @PathVariable Long commentId,
--- ewm-service/src/main/java/ru/practicum/mainmicroservice/service/priv/PrivateUserCommentService.java
+++ ewm-service/src/main/java/ru/practicum/mainmicroservice/service/priv/PrivateUserCommentService.java
@@ -1,41 +1,54 @@
 package ru.practicum.mainmicroservice.service.priv;
 
+import org.springframework.data.domain.PageRequest;
 import org.springframework.stereotype.Service;
+import ru.practicum.mainmicroservice.dto.CommentDto;
+import ru.practicum.mainmicroservice.dto.CommentMapper;
+import ru.practicum.mainmicroservice.exception.NotFoundException;
+import ru.practicum.mainmicroservice.model.Comment;
+import ru.practicum.mainmicroservice.model.Event;
+import ru.practicum.mainmicroservice.model.User;
+import ru.practicum.mainmicroservice.repository.CommentRepository;
+import ru.practicum.mainmicroservice.repository.EventRepository;
+import ru.practicum.mainmicroservice.repository.UserRepository;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 @Service
 public class PrivateUserCommentService {
-//    CommentRepository commentRepository;
-//    UserRepository userRepository;
-//    EventRepository eventRepository;
-//
-//    public PrivateUserCommentService(CommentRepository commentRepository, UserRepository userRepository,
-//                                     EventRepository eventRepository) {
-//        this.commentRepository = commentRepository;
-//        this.userRepository = userRepository;
-//        this.eventRepository = eventRepository;
-//    }
-//
-//
-//    public CommentDto saveComment(Long userId, Long eventId, CommentDto commentDto) {
-//        User user = userRepository.findById(userId).orElseThrow(() ->
-//                new NotFoundException(" Пользователь не существует"));
-//        Event event = eventRepository.findById(eventId).orElseThrow(() ->
-//                new NotFoundException("Событие не существует"));
-//        Comment comment = CommentMapper.toComment(commentDto);
-//        comment.setUser(user);
-//        comment.setEvent(event);
-//        return CommentMapper.toCommentDto(commentRepository.save(comment));
-//    }
-//
-//
-//    public List<CommentDto> getAllCommentsByUser(Long userId, int from, int size) {
-//        User user = userRepository.findById(userId).orElseThrow(() ->
-//                new NotFoundException(" Пользователь не существует"));
-//        return commentRepository.findAllByUser(user, PageRequest.of(from / size, size))
-//                .stream()
-//                .map(CommentMapper::toCommentDto)
-//                .collect(Collectors.toList());
-//    }
+    CommentRepository commentRepository;
+    UserRepository userRepository;
+    EventRepository eventRepository;
+
+    public PrivateUserCommentService(CommentRepository commentRepository, UserRepository userRepository,
+                                     EventRepository eventRepository) {
+        this.commentRepository = commentRepository;
+        this.userRepository = userRepository;
+        this.eventRepository = eventRepository;
+    }
+
+
+    public CommentDto saveComment(Long userId, Long eventId, CommentDto commentDto) {
+        User user = userRepository.findById(userId).orElseThrow(() ->
+                new NotFoundException(" Пользователь не существует"));
+        Event event = eventRepository.findById(eventId).orElseThrow(() ->
+                new NotFoundException("Событие не существует"));
+        Comment comment = CommentMapper.toComment(commentDto);
+        comment.setUser(user);
+        comment.setEvent(event);
+        return CommentMapper.toCommentDto(commentRepository.save(comment));
+    }
+
+
+    public List<CommentDto> getAllCommentsByUser(Long userId, int from, int size) {
+        User user = userRepository.findById(userId).orElseThrow(() ->
+                new NotFoundException(" Пользователь не существует"));
+        return commentRepository.findAllByUser(user, PageRequest.of(from / size, size))
+                .stream()
+                .map(CommentMapper::toCommentDto)
+                .collect(Collectors.toList());
+    }
 //
 //
 //

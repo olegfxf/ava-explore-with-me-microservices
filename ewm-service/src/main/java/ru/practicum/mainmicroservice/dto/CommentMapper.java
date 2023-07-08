31ee4555303174b8ada5f9cbedb1460300a19cee
Oleg <ovasilyevfx@mail.ru>
2023-07-08 17:25:59 +0300

    fix: Замечания по проекту

--- ewm-service/src/main/java/ru/practicum/mainmicroservice/dto/CommentMapper.java
+++ ewm-service/src/main/java/ru/practicum/mainmicroservice/dto/CommentMapper.java
@@ -1,15 +1,17 @@
 package ru.practicum.mainmicroservice.dto;
 
+import lombok.experimental.UtilityClass;
 import ru.practicum.mainmicroservice.model.Comment;
 
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 
+@UtilityClass
 public class CommentMapper {
 
     static DateTimeFormatter dateTimeFormatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss");
 
-    public static CommentDto toCommentDto(Comment comment) {
+    public CommentDto toCommentDto(Comment comment) {
         return CommentDto.builder()
                 .id(comment.getId())
                 .text(comment.getText())
@@ -18,7 +20,7 @@ public class CommentMapper {
                 .build();
     }
 
-    public static Comment toComment(CommentDto commentDto) {
+    public Comment toComment(CommentDto commentDto) {
         return Comment.builder()
                 .text(commentDto.getText())
                 .created(LocalDateTime.now())
